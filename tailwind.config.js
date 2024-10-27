a0d06dc11faa24de3c501212204c3066165aa964
Vincent Madayanto <vincentmadayanto@gmail.com>
2024-10-27 11:07:07 +0700

    first init

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./index.html", // Ensure you specify the correct paths to your HTML files
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+      },
+      colors: {
+        customBlack: "#212121",
+      },
+    },
+  },
+};
